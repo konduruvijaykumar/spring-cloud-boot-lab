8ecef807400326e43d41506e2870fac4b3eeec33
Vijay Konduru <vkonduru3@csc.com>
2017-04-19 15:22:48 +0530

    Vijay - 2017/04/19 - spring cloud, boot, microservices examples

---
+++ lab4-concise/restservices-all-solution/src/main/java/org/pjay/RestservicesAllSolutionApplication.java
@@ -0,0 +1,12 @@
+package org.pjay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicesAllSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestservicesAllSolutionApplication.class, args);
+	}
+}

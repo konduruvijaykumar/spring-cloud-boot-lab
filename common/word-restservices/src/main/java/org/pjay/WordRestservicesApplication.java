ab440c497c39856391ef44883ba31072db9cb2dc
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-04-23 05:12:05 +0530

    Vijay - 2017/04/23 - spring cloud, boot, microservices examples

---
+++ common/word-restservices/src/main/java/org/pjay/WordRestservicesApplication.java
@@ -0,0 +1,12 @@
+package org.pjay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordRestservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordRestservicesApplication.class, args);
+	}
+}

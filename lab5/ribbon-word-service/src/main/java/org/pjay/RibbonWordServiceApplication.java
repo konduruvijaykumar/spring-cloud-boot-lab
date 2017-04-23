4c1c269b9aec5af7733c44b38bb3f3c0d488305a
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-04-23 11:58:01 +0530

    Vijay - 2017/04/23 - spring cloud, boot, microservices examples

---
+++ lab5/ribbon-word-service/src/main/java/org/pjay/RibbonWordServiceApplication.java
@@ -0,0 +1,12 @@
+package org.pjay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonWordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonWordServiceApplication.class, args);
+	}
+}

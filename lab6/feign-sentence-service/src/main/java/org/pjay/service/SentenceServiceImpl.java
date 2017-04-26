e9e22ec28c2763b410c82d8405c2aa8e2d92535d
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-04-26 07:08:31 +0530

    Vijay - 2017/04/26 - spring cloud, boot, microservices examples

--- lab6/feign-sentence-service/src/main/java/org/pjay/service/SentenceServiceImpl.java
+++ lab6/feign-sentence-service/src/main/java/org/pjay/service/SentenceServiceImpl.java
@@ -3,22 +3,61 @@
  */
 package org.pjay.service;
 
+import org.pjay.dao.WordDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * @author vijayk
  *
- * Build a sentence by assembling randomly generated subjects, verbs, 
- * articles, adjectives, and nouns.  The individual parts of speech will 
+ * Build a sentence by assembling randomly generated subjects, verbs,
+ * articles, adjectives, and nouns. The individual parts of speech will
  * be obtained by calling the various DAOs.
  * 
  */
 @Service
 public class SentenceServiceImpl implements SentenceService {
 
+	private WordDao verbRestService;
+	private WordDao subjectRestService;
+	private WordDao articleRestService;
+	private WordDao adjectiveRestService;
+	private WordDao nounRestService;
+
 	@Override
 	public String buildSentence() {
-		return null;
+		String sentence = "There was a problem assembling the sentence!";
+		sentence = String.format("%s %s %s %s %s.", subjectRestService.getWord().getWord(),
+				verbRestService.getWord().getWord(), articleRestService.getWord().getWord(),
+				adjectiveRestService.getWord().getWord(), nounRestService.getWord().getWord());
+		return sentence;
+	}
+
+	// @Autowired on variables without setter should also work. Please verify
+
+	@Autowired
+	public void setVerbRestService(WordDao verbRestService) {
+		this.verbRestService = verbRestService;
+	}
+
+	@Autowired
+	public void setSubjectRestService(WordDao subjectRestService) {
+		this.subjectRestService = subjectRestService;
+	}
+
+	@Autowired
+	public void setArticleRestService(WordDao articleRestService) {
+		this.articleRestService = articleRestService;
+	}
+
+	@Autowired
+	public void setAdjectiveRestService(WordDao adjectiveRestService) {
+		this.adjectiveRestService = adjectiveRestService;
+	}
+
+	@Autowired
+	public void setNounRestService(WordDao nounRestService) {
+		this.nounRestService = nounRestService;
 	}
 
 }

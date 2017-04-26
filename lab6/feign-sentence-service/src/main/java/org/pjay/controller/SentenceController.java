9135b9f12029cf786190081f0c6e72e83307379d
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-04-26 06:11:27 +0530

    Vijay - 2017/04/26 - spring cloud, boot, microservices examples

--- lab6/feign-sentence-service/src/main/java/org/pjay/controller/SentenceController.java
+++ lab6/feign-sentence-service/src/main/java/org/pjay/controller/SentenceController.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package org.pjay;
+package org.pjay.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;

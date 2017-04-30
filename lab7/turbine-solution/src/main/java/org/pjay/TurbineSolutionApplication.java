284d3fdd6c2d3b5d08f2cd2b3b6c7c8896439a4b
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-04-30 07:13:39 +0530

    Vijay - 2017/04/30 - spring cloud, boot, microservices examples

---
+++ lab7/turbine-solution/src/main/java/org/pjay/TurbineSolutionApplication.java
@@ -0,0 +1,12 @@
+package org.pjay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbineSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineSolutionApplication.class, args);
+	}
+}

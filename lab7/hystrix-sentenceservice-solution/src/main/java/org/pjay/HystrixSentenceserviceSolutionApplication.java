c8b68d880bf1395442df330774ea6ccd8f380df4
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-04-30 12:05:25 +0530

    Vijay - 2017/04/30 - spring cloud, boot, microservices examples

--- lab7/hystrix-sentenceservice-solution/src/main/java/org/pjay/HystrixSentenceserviceSolutionApplication.java
+++ lab7/hystrix-sentenceservice-solution/src/main/java/org/pjay/HystrixSentenceserviceSolutionApplication.java
@@ -2,8 +2,16 @@ package org.pjay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class HystrixSentenceserviceSolutionApplication {
 
 	public static void main(String[] args) {

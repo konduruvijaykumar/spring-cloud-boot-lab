c9dabb3e5dcb1c5232cc20d2e0cde5063c6eeb1f
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-05-01 08:30:41 +0530

    Vijay - 2017/05/1 - spring cloud, boot, microservices examples

--- lab7/turbine-solution/src/main/java/org/pjay/TurbineSolutionApplication.java
+++ lab7/turbine-solution/src/main/java/org/pjay/TurbineSolutionApplication.java
@@ -12,6 +12,16 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
  * 
  * https://docs.spring.io/spring-boot/docs/current/reference/html/common-application-properties.html
  * management.port= # Management endpoint HTTP port. Uses the same port as the application by default. Configure a different port to use management-specific SSL.
+ * 
+ * TODO:
+ * Not working properly, need to verify this
+ * http://cloud.spring.io/spring-cloud-netflix/spring-cloud-netflix.html
+ * https://github.com/Netflix/Turbine/wiki/Configuration-(1.x)
+ * https://github.com/spring-cloud-samples/turbine
+ * https://dzone.com/articles/spring-cloud-with-turbine
+ * http://www.java-allandsundry.com/2016/01/spring-cloud-with-turbine.html
+ * http://www.java-allandsundry.com/2016/05/spring-cloud-with-turbine-amqp.html
+ * 
  */
 @SpringBootApplication
 @EnableTurbine

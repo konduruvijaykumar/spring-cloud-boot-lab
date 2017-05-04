c69006bff3b287821d8b45b856bc775621ffcc94
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-05-04 06:53:52 +0530

    Vijay - 2017/05/4- spring cloud, boot, microservices examples

---
+++ lab9/gateway-wordservices/src/main/java/org/pjay/GatewayWordservicesApplication.java
@@ -0,0 +1,12 @@
+package org.pjay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayWordservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayWordservicesApplication.class, args);
+	}
+}

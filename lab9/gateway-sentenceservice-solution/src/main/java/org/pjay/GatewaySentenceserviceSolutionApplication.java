222b99946619c6b48efe5d048c32b517bd4cd51c
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-05-05 13:03:29 +0530

    Vijay - 2017/05/05 - spring cloud, boot, microservices examples

--- lab9/gateway-sentenceservice-solution/src/main/java/org/pjay/GatewaySentenceserviceSolutionApplication.java
+++ lab9/gateway-sentenceservice-solution/src/main/java/org/pjay/GatewaySentenceserviceSolutionApplication.java
@@ -1,8 +1,12 @@
 package org.pjay;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,4 +15,16 @@ public class GatewaySentenceserviceSolutionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewaySentenceserviceSolutionApplication.class, args);
 	}
+	
+	// Not need for this project
+	// Etags used for caching data, instead of rebuilding response.
+	// When server sends response for the first time, it calculates hash for the object and sends it to browser via etags
+	// When browser requests for the object again the server will verify if browser sent hash via etag is same for the object response produced
+	// If so the the server does not transmit the bulky object, instead sends 304 which indicates browser that the object or resource is not modified
+	// So browser will use the existing. This reduces the bulky object transfer via network
+	// https://httpstatuses.com/304 -- not modified
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }    
 }

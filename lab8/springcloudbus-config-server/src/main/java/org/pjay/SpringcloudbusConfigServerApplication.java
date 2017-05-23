159e07a11ce87f0974e6d81f08b07291185bf66e
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-05-23 15:29:32 +0530

    Vijay - 2017/05/23 - spring cloud bus in microservices examples

---
+++ lab8/springcloudbus-config-server/src/main/java/org/pjay/SpringcloudbusConfigServerApplication.java
@@ -0,0 +1,12 @@
+package org.pjay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudbusConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudbusConfigServerApplication.class, args);
+	}
+}
